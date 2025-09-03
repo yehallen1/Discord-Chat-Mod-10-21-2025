880ad2748c23de23ae261d51998845369e2fd6ad
Ryan McGough <132298917+rhmcgough@users.noreply.github.com>
2025-09-03 08:51:32 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py
+
+# Just making this file so that I have something to clone onto my ubuntu machine. 
+# 
+#
+#  TODO: 1. Create general idea for the bot. Ideally it should have a command line that can be used within discord. 
+#           Additional functions should be used as well. 
+#  TODO 2:  Find libraries we can use for the bot. Github should have hella options. 
