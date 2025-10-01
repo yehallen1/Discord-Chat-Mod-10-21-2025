223d69a036f06fb21f5e9d8c870962c8fb729123
yehallen1 <yehallen1@gmail.com>
2025-10-01 08:47:39 -0700

    Update playlist.py

--- MusicPlayer/playlist.py
+++ MusicPlayer/playlist.py
@@ -1,2 +1,13 @@
 import random
     """For now I plan to use this to store the queue of songs"""
+from collections import deque
+class Playlist:
+    def __init__(self):
+        #Dual ended queues to remove from the front and back quickly
+        self.playlist = deque()
+        self.playlist_history = deque()
+        self.track_history = deque()
+        self.looping = False
+
+    def get_len(self):
+        return len(self.playlist)
