7b6aa9531eefcf6ef192b28cf010b3ce13406e77
Kevin Qi <kevinJp6526@csu.fullerton.edu>
2025-10-07 20:19:52 -0700

    updated the on_message event, added /level command

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import discord
+import leveling_sys as lvl
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 from discord import Member
 from discord.ext.commands import has_permissions, MissingPermissions
 from dotenv import load_dotenv 
-from leveling_sys import Adding_Xp
+
 
 # Token stuff
 load_dotenv()
@@ -21,14 +22,28 @@ intents.members = True
 
 client = commands.Bot(command_prefix="/", intents=intents)
 
+
+# initialize database on startup
 @client.event
 async def on_ready():
+    await lvl.init_db()
     print("Ready")
+
 
 @client.command()
 async def test(ctx):
     await ctx.send("Test")
 
+# ========================================
+# Leveling system
+# =========================================
+
+# /level command to print XP and Level
+@client.command()
+async def level(ctx):
+    user_id = str(ctx.author.id)
+    xp, level = await lvl.print_level(user_id)
+    await ctx.send(f"Hello, {ctx.author.name}!\nYour current Level is {level} and Xp is at {xp}")
 
 
 # used to detect messages for adding XP to data base
@@ -38,28 +53,37 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    # ignore '/' commands
+    # ignore / commands otherwise gives XP still
     if message.content.startswith("/"):
+        await client.process_commands(message)
         return
 
     # fetch the user's id 
-    UserId = message.author.id
-    Adding_Xp(UserId, 10)
+    UserId = str(message.author.id)
+    await lvl.add_xp(UserId, 10)
 
+    # Allow other bot commands (like /level or /test) to still work after this event
+    await client.process_commands(message)
 
 
 # Detects when someone joins the server.
 # Kevin this might be useful to keep metrics on when someone joins the server. 
 @client.event
 async def on_member_join(member):
+    # add user_id to the datbase on join
+    user_id = str(member.id)
     await member.send()
 
 @client.event
 async def on_member_remove(member):
     await member.send()
 
-# For playing audio in voice calls. 
 
+# ==========================
+# Music Playback
+# ==========================
+
+# For playing audio in voice calls. 
 @client.command()
 @has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
